d605df9e1e42ba96b213e40273d72e832ab9fa6c
Isuru <izurumw@gmail.com>
2023-11-06 11:22:23 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,75 @@
+// document.getElementById("hi1").style.color="yellow";
+
+
+// // stogate
+// const x=6
+// localStorage.setItem("valx",x);
+// sessionStorage.setItem("valy",x)
+// console.log(sessionStorage.setItem("valyss",x));
+
+// localStorage.clear();
+
+// const g=25;
+// const h=33;
+// const r=3;
+// if(g===h){
+//     console.log('gg');
+// }
+// else if(g===r){
+//     console.log("rr")
+// }else{
+//     console.log("fa")
+// }
+
+// switch (h){
+//     case 5:
+//         console.log("5");    
+//         break;
+
+//     case 7:
+//         console.log("7");
+//         break;
+
+//     case 3:
+//         console.log("3");
+//     default:
+//         console.log('false')
+// }
+
+// for(let i=0;i<=10;i++){
+//     console.log(i);
+// }
+
+
+// var a=6;
+// while(a<=10){
+//     console.log(a);
+//     a++;
+// }
+
+// var d=2;
+// do{
+//     console.log(d)
+// d++;
+
+// }while (d<=15);
+
+// const array =[
+//     {
+//         val1:"1",
+//         val2:"2",
+//     },
+//     {
+//         val1:"3",
+//         val4:"4",
+//     },
+//     {
+//         val1:"5",
+//         val6:"6",
+//     },
+// ];
+// array.forEach(function(value){
+//     console.log(value.val1);
+// })
+
+

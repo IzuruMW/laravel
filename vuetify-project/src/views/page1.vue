e7e50b1e5de33e28dcf728b8a68ae5372a9d0a5f
Isuru <izurumw@gmail.com>
2023-11-09 11:50:00 +0530

    import component

--- vuetify-project/src/views/page1.vue
+++ vuetify-project/src/views/page1.vue
@@ -1,10 +1,21 @@
 <template>
     <center>  
 <h1>page1</h1>
+<page2/>;
+<!-- <Home/>; -->
 </center>
 </template>
 <script>
-export default{};
+import page2 from "@/views/page2.vue";
+// import Home from "@/views/Home.vue";
+
+
+export default{
+  components :{
+    page2,
+    // Home
+  }
+};
 
 
 </script>
--- vuetify-project/src/views/page2.vue
+++ vuetify-project/src/views/page2.vue
@@ -1,8 +1,17 @@
 <template>
     <center>
 <h1>page 2</h1>
+<!-- <page1/> -->
+
 </center>
 </template>
 <script>
-export default{};
+// import page1 from "@/views/page1.vue"
+
+export default{
+    // components :{
+    //     page1
+    // }
+    
+};
 </script>

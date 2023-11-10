c9d3c3a8c2a8ca461481d95a4d9dbe5aebba7724
Isuru <izurumw@gmail.com>
2023-11-10 13:16:21 +0530

    add grid images

--- vuetify-project/src/views/Home.vue
+++ vuetify-project/src/views/Home.vue
@@ -1,44 +1,48 @@
 <template>
-  <center> 
+  <center>
+    <h1>Hello world!</h1>
+    <v-row>
+      <v-column>
+        
+      </v-column>
+    </v-row>
 
- <h1> Hello world!</h1>
+    <div>
+      <button @click="directPage()">submit page1</button>
+      <br /><br />
+      <button @click="directPage12()">submit page2</button> <br /><br />
+      <button @click="abc()">submit page2</button>
+    </div>
 
- <div>
- <button @click=" directPage()">submit page1</button>
- <br><br>
- <button @click=" directPage12()">submit page2</button> <br><br>
- <button @click="abc()"> submit page2</button>
- </div>
-
-
-<page1/>
-<page2/>
-</center>
+    <page1 />
+    <page2 />
+  </center>
 </template>
 
-<script >
+<script>
 import page1 from "@/views/page1.vue";
-import page2 from"@/views/page2.vue";
-export default{
-  methods:{
-directPage()
-{  this.$router.push('page1'); 
-
-},
-directPage12(){
-  this.$router.push('page1');
- },
- abc(){
-  this.$router.push('page2');
- },
- 
-},
-components :{
-  page1,
-  page2
-  
-}
-
+import page2 from "@/views/page2.vue";
+export default {
+  methods: {
+    directPage() {
+      this.$router.push("page1");
+    },
+    directPage12() {
+      this.$router.push("page1");
+    },
+    abc() {
+      this.$router.push("page2");
+    },
+  },
+  components: {
+    page1,
+    page2,
+  },
+  beforeCreate() {
+    console.log("Isuru");
+  },
+  created() {
+    console.log("created");
+  },
 };
-
 </script>
--- vuetify-project/src/views/page1.vue
+++ vuetify-project/src/views/page1.vue
@@ -1,20 +1,52 @@
 <template>
     <center>  
-<h1>page1</h1>
-<page2/>;
+
+
+<v-row>
+ <v-col cols="12" sm="4" class="bg-red pa-5" > col 1 </v-col>
+ <v-col  cols="12" sm="4" class="bg-yellow pa-5 "> col 2 </v-col>
+ <v-col  cols="12" sm="4" class="bg-blue pa-5"> col 3 </v-col>
+ <!-- <v-col cols="12" sm="4"> col 4 </v-col> -->
+
+ 
+
+</v-row>
+
+
+
+
+
+<!-- <page2/>; -->
+
+
+<v-img
+  :width="300"
+  aspect-ratio="16/9"
+  cover
+  src="../assets/images/u131.png"
+></v-img>
+
+<v-row>
+  <v-col cols="12" sm="2" ><v-img src="../assets/images/u131.png" width="300"></v-img> </v-col>
+  <v-col cols="12" sm="2" style="width: 300px;"><v-img src="../assets/images/u11.png" width="300"></v-img></v-col>
+  <v-col cols="12" sm="2" style="width: 300px;"><v-img src="../assets/images/u29.png" width="300"></v-img></v-col>
+  <v-col cols="12" sm="2" style="width: 300px;"><v-img src="../assets/images/u23.png" width="300"></v-img></v-col>
+  <v-col  cols="12" sm="2" style="width: 300px;"><v-img src="../assets/images/u5.png"  width="300"></v-img></v-col>
+  <v-col  cols="12" sm="2" style="width: 300px;"><v-img src="../assets/images/u41.png"  width="300"></v-img></v-col>
+</v-row>
 <!-- <Home/>; -->
 </center>
 </template>
 <script>
-import page2 from "@/views/page2.vue";
+// import page2 from "@/views/page2.vue";
 // import Home from "@/views/Home.vue";
 
 
 export default{
-  components :{
-    page2,
-    // Home
-  }
+  // components :{
+  //   page2,
+  //   // Home
+  // }
 };
 
 
